b4b2284354f78139950618df97b955851139a755
soatmurotov1 <soatmurotovabrorbek23@gmail.com>
2025-08-15 22:29:50 +0500

    Fayl qushuldi

---
+++ main.js
@@ -0,0 +1,115 @@
+
+
+
+
+
+
+
+// let age = 16 
+
+// if (age >= 18){
+//     console.log("ADULT");
+// }else {
+//     console.log("MINOR");
+    
+// }
+
+
+// console.log(name);
+// console.log(typeof name);
+
+
+/*  
+    1.Number 
+    let namberAge = Number(age)
+    console.log(typeof numberAge)
+
+*/ 
+/*  
+    2.parseInt
+    let parseInt(age)
+    console.log(numberAge)
+    console.log(typeof numberAge)
+
+*/ 
+/*  
+    3.+
+    let numberAge = +age
+    console.log(numberAge)
+    console.log(typeof numberAge)
+    
+ */
+
+// function hafta(){
+//     let son = +prompt("Hafta kunini kiriting: ")
+//     if (son == 1){
+//         alert("Dushanba")
+//     }
+//     else if(son == 2){
+//         alert("Seshanba")
+//     }
+//     else if(son == 3){
+//         alert("Choshanba")
+//     }
+//     else if(son == 4){
+//         alert("Payshanba")
+//     }
+//     else if(son == 5){
+//         alert("Juma")
+//     }
+//     else if(son == 6){
+//         alert("Shanba")
+//     }
+//     else if(son == 7){
+//         alert("Yakshanba")
+//     }else{
+//         alert("Bunday kun yuq.")
+//     }
+// }
+// hafta()
+
+
+
+
+
+// function vaqt(){
+//     let vaqt = 129
+//     let min = []
+//     let secund =[]
+//     let min1 = Math.vaqt / 60
+//     let secund1 = vaqt % 60 
+//     console.log(`${min1}:${secund1}`)
+
+// }
+// vaqt()
+
+
+// let person = {name: "Abrorbek", age:19}
+// for (let key in person){
+//     console.log(key + ": " + person[key]);
+
+// }
+
+
+
+// let numbers = [1,2,3,4,5]
+// for (let number of numbers){
+//     console.log(number)
+// }
+
+
+// let num = 0
+// if ( num == "0"){
+//     console.log("Loose dfsbg ");
+// }
+
+// if (num === "0") {
+//     console.log("fbgn arb ");
+    
+// }else{
+//     console.log("Strrict equ");
+    
+// }
+
+
+
